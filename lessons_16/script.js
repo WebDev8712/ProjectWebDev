abcb36946b263db62adc75db91792ab1bfd717d9
Adik8712 <taukelevadil@gmail.com>
2024-03-14 02:52:50 +0500

    update

---
+++ lessons_16/script.js
@@ -0,0 +1,17 @@
+let links = document.querySelectorAll("header a");
+links.forEach(link => {
+    link.addEventListener("click", async function (event) {
+        event.preventDefault();
+        let href = this.href;
+        let data = await fetch(href);
+        document.querySelector("#container").innerHTML = await data.text();
+    });
+});
+
+async function start() {
+    
+    let data = await fetch("main.html");
+    document.querySelector("#container").innerHTML = await data.text();
+}
+
+start()
